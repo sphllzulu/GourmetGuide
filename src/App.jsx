8de9dad6a4296b025159da1e0adecd5feab83ce7
Siphelele <sphllzulu@gmail.com>
2025-01-04 17:12:12 +0200

    wrapped the auth provider on the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import Home from './Home';
+import { AuthProvider } from './AuthContext';
 
 function App() {
   return (
+    <AuthProvider>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -12,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
+    </AuthProvider>
   );
 }
 
